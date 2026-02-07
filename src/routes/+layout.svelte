4c1b2ac7ef74de5713cbdb5fd2d287a9f199eae6
Lanzin Code <lanzincode@gmail.com>
2026-02-06 21:35:41 -0300

    chore: adicionados estrutura de pastas básica e estilos básicos

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import favicon from '/assets/favicon-32x32.png';
+	import '$lib/styles/tokens.css';
+	import '$lib/styles/base.css';
+	import '$lib/styles/layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Frontend Mentor | E-commerce product page</title>
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Header from '$lib/components/header/Header.svelte'
+</script>
+
+<Header />
+
+<main>
+  <h2>main page</h2>
+</main>
